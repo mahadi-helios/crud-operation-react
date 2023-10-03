f76a30f3b85307142663f3c03fa5126a54d8ce58
mahadi-helios <md.mahadihyperhelious@gmail.com>
2023-10-03 09:31:43 +0600

    some comment

--- django crude/config/settings.py
+++ django crude/config/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # add this 
 ]
 
 ROOT_URLCONF = 'config.urls'
