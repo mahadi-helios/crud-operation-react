7e6b8b34b461c1c790af723f994da2337a4e7e87
mahadi-helios <md.mahadihyperhelious@gmail.com>
2023-10-05 18:09:11 +0600

    change design

--- react crud/src/Pages/ContactList.jsx
+++ react crud/src/Pages/ContactList.jsx
@@ -2,30 +2,39 @@ import React from "react";
 
 function ContactList({ contacts, navigate, removeContact }) {
   return (
-    <div>
-      <h1 className="contact-header">Contact List</h1>
-      {contacts.length > 0 ? (
-        <div className="posts-div">
-          {contacts.map((contact) => (
-            <div className="post-data " key={contact.id}>
-              <h4 className="name">Name: {contact.name}</h4>
-              <p className="phone-number">Number: {contact.phone_number}</p>
-              <button
-                className="update-btn"
-                onClick={() => navigate(`/edit-user/${contact.id}`, { state: contact })}
-              >
-                Update
-              </button>
-              <button className="delete-btn" onClick={() => removeContact(contact.id)}>
-                Delete
-              </button>
+    <>
+      <div className="contact-div">
+        <h1 className="contact-header">Contact List</h1>
+        {contacts.length > 0 ? (
+          <div className="table-container">
+            <div class="table">
+              <div className="table-row header">
+                <div className="table-cell">Name</div>
+                <div className="table-cell">Number</div>
+                <div className="table-cell">Action</div>
+              </div>
+              {contacts.slice(0,10).map((contact) => (
+              <div className="table-row" key={contact.id}>
+                <div className="table-cell">{contact.name}</div>
+                <div className="table-cell">{contact.phone_number}</div>
+                <div className="table-cell">
+                  <button className="update-btn" onClick={() => navigate(`/edit-user/${contact.id}`, { state: contact })}>
+                    Update
+                  </button>
+                  <button className="delete-btn" onClick={() => removeContact(contact.id)}>
+                    Delete
+                  </button>
+                </div>
+              </div>
+              ))}
             </div>
-          ))}
-        </div>
-      ) : (
-        <p>No Contact Here</p>
-      )}
-    </div>
+          </div>  
+        ) : (
+          <p className="no-contact">No Contact Here</p>
+        )}
+      </div>
+   
+   </>
   );
 }
 
--- react crud/src/Pages/UserForm.jsx
+++ react crud/src/Pages/UserForm.jsx
@@ -2,45 +2,44 @@ import React from "react";
 
 function UserForm({ inputValues, handleFormSubmit, handleInputChange, resetForm, alert }) {
   return (
-    <form onSubmit={handleFormSubmit}>
-      <h1 className="form-header">User {inputValues.id ? "Edit" : "Create"} Information</h1>
-      <div className="label1">
-        {/* <label  className="label-back" htmlFor="username">Username:</label> */}
-        <input
-          type="text"
-          name="name"
-          placeholder="Username"
-          value={inputValues.name}
-          onChange={handleInputChange}
-          id="username" 
-          required
-        />
-      </div>
-      <div className="label1">
-        {/* <label  className="label-back" htmlFor="usernumber">User Number (BD Only - 11 digits):</label> */}
-        <input
-          type="text"
-          name="number"
-          placeholder="User Number (BD Only - 11 digits)"
-          value={inputValues.number}
-          onChange={handleInputChange}
-          id="usernumber"
-          required
-        />
-      </div>
-      {alert && (
-        <div className={`alert ${alert.type} danger ,success `}>
-          {alert.message}
+    <>
+    <div className="form-div">
+      <form onSubmit={handleFormSubmit}>
+        <h1 className="form-header">User {inputValues.id ? "Edit" : "Create"} Information</h1>
+        <div className="label1">
+          <input
+            type="text"
+            name="name"
+            placeholder="Username"
+            value={inputValues.name}
+            onChange={handleInputChange}
+            id="username"
+            required
+          />
         </div>
-      )}
-      <br />
-      <button className="button1" type="submit">
-        Submit
-      </button> 
-      {/* <button className="btn" onClick={() => resetForm()}>
-        Clear
-      </button> */}
-    </form>
+        <div className="label1">
+          <input
+            type="text"
+            name="number"
+            placeholder="User Number (BD Only - 11 digits)"
+            value={inputValues.number}
+            onChange={handleInputChange}
+            id="usernumber"
+            required
+          />
+        </div>
+        {alert && (
+          <div className={`alert ${alert.type}`}>
+            {alert.message}
+          </div>
+        )}
+        <br />
+        <button className="button1" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+    </>
   );
 }
 
